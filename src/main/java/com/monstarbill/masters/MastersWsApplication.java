f1856c12e453b584e3a95d34323f9b42f7f43087
vidhateprashant <vidhate.prashant98@gmail.com>
2023-02-19 14:08:56 +0530

    cors

--- src/main/java/com/monstarbill/masters/MastersWsApplication.java
+++ src/main/java/com/monstarbill/masters/MastersWsApplication.java
@@ -8,6 +8,8 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import feign.Logger;
 
@@ -31,4 +33,15 @@ public class MastersWsApplication {
 		return new BCryptPasswordEncoder();
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+					.allowedOrigins("http://43.205.33.156:8080")
+					.allowedOrigins("http://43.205.33.156:8082");
+			}
+		};
+	}
 }
